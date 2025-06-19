4e6415f29b294f558b1b884e752487922c2979fe
Chen21yan <1170860535@qq.com>
2025-06-19 11:59:53 +0800

    数据库数据

--- app01/views.py
+++ app01/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from app01 import models
 
 # Create your views here.
 def depart_list(request):
     """ 部门列表 """
-    return render(request, 'depart_list.html')
+
+    # 去数据库中获取所有的部门列表
+    #  [对象,对象,对象]
+    queryset = models.Department.objects.all()
+
+    return render(request, 'depart_list.html', {'queryset': queryset})
