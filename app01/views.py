ad255aca867cb8da6806f61c4db627a4c08a0bc8
Chen21yan <1170860535@qq.com>
2025-06-22 14:28:08 +0800

    靓号管理-搜索

--- app01/views.py
+++ app01/views.py
@@ -197,11 +197,37 @@ def user_delete(request, nid):
 
 def pretty_list(request):
     """ 靓号列表 """
+    data_dict = {}
+    search_data = request.GET.get('q', "")
+    if search_data:
+        data_dict["mobile__contains"] = search_data
 
-    # select * from 表 order by level desc;
-    queryset = models.PrettyNum.objects.all().order_by("-level")
+    queryset = models.PrettyNum.objects.filter(**data_dict).order_by("-level")
 
-    return render(request, 'pretty_list.html', {'queryset': queryset})
+    return render(request, 'pretty_list.html', {'queryset': queryset, 'search_data': search_data})
+
+
+    # # 搜索手机号方式一
+    # q1 = models.PrettyNum.objects.filter(mobile="13945777890", id=3)
+    # print(q1)
+    #
+    # # 搜索手机号方式二
+    # data_dict = {"mobile":"13526789231", "id":2}
+    # q2 = models.PrettyNum.objects.filter(**data_dict)
+    # print(q2)
+
+
+    # models.PrettyNum.objects.filter(id=3)      #等于3
+    # models.PrettyNum.objects.filter(id__gt=3)   # 大于3
+    # models.PrettyNum.objects.filter(id__gte=3)  # 大于等于3
+    # models.PrettyNum.objects.filter(id__lt=3)   # 小于3
+    # models.PrettyNum.objects.filter(id__lte=3)  # 小于等于3
+
+    # models.PrettyNum.objects.filter(mobile__startswith="1399")   #筛选出以1399开头
+    # models.PrettyNum.objects.filter(mobile__endswith="890")      #筛选出以890结尾
+    # data_dict = {"mobile__contains": "999", "id__gt": 2}
+    # q2 = models.PrettyNum.objects.filter(**data_dict)
+    # models.PrettyNum.objects.filter(mobile__contains="999")      #筛选出包含999
 
 
 from django.core.validators import RegexValidator
