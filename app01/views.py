18892d685164a19870960b9ac47a078502140ed2
Chen21yan <1170860535@qq.com>
2025-06-29 17:39:03 +0800

    分页组件封装

--- app01/views.py
+++ app01/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from app01 import models
 from app01.models import PrettyNum
 from django.utils.safestring import mark_safe
+from app01.utils.pagination import Pagination
 
 
 # Create your views here.
@@ -208,90 +209,15 @@ def pretty_list(request):
     if search_data:
         data_dict["mobile__contains"] = search_data
 
-    # 1、根据用户想要访问的页码、计算出起止位置
-    page = int(request.GET.get('page', 1))
-    page_size = 10
-    start = (page - 1) * page_size
-    end = start + page_size
+    queryset = models.PrettyNum.objects.filter(**data_dict).order_by("-level")
 
-    queryset = models.PrettyNum.objects.filter(**data_dict).order_by("-level")[start:end]
+    page_object = Pagination(request, queryset, page_size=15)
 
-    # 数据总条数
-    total_count = models.PrettyNum.objects.filter(**data_dict).order_by("-level").count()
-
-    # 总页码
-    total_page_count, div = divmod(total_count, page_size)
-    if div:
-        total_page_count += 1
-
-    # 计算出，显示当前页的前5页、后5页
-    plus = 5
-    if total_page_count <= 2 * plus + 1:
-        # 数据库中数据少，没达到11页
-        start_page = 1
-        end_page = total_page_count
-    else:
-        # 当前页<5
-        if page <= plus:
-            start_page = 1
-            end_page = 2 * plus + 1
-        else:
-            # 当前页+5 > 总页面
-            if (page + plus) > total_page_count:
-                start_page = total_page_count - 2 * plus
-                end_page = total_page_count
-            else:
-                start_page = page - plus
-                end_page = page + plus
-
-    # 页码
-    page_str_list = []
-
-    # 首页
-    page_str_list.append('<li><a href="?page={}">首页</a></li>'.format(1))
-
-    # 上一页
-    if page > 1:
-        prev = '<li><a href="?page={}">上一页</a></li>'.format(page - 1)
-    else:
-        prev = '<li><a href="?page={}">上一页</a></li>'.format(1)
-    page_str_list.append(prev)
-
-    # 页面
-    for i in range(start_page, end_page+1):
-        if i == page:
-            els = '<li class="active"><a href="?page={}">{}</a></li>'.format(i, i)
-        else:
-            els = '<li><a href="?page={}">{}</a></li>'.format(i, i)
-        page_str_list.append(els)
-
-    # 下一页
-    if page < total_page_count:
-        prev = '<li><a href="?page={}">下一页</a></li>'.format(page + 1)
-    else:
-        prev = '<li><a href="?page={}">下一页</a></li>'.format(total_page_count)
-    page_str_list.append(prev)
-
-    # 尾页
-    page_str_list.append('<li><a href="?page={}">尾页</a></li>'.format(total_page_count))
-
-    search_string = """
-    <li>
-        <form style="float:left; margin-left: -1px" method="GET">
-            <input name="page"
-                    style="position: relative;float:left; display: inline-block; width: 88px; border-radius: 0;"
-                    type="text" class="form-control" placeholder="页码">
-            <button style="border-radius: 0" class="btn btn-default" type="submit">跳转</button>
-        </form>
-    </li>
-    """
-
-    page_str_list.append(search_string)
-
-    page_string = mark_safe(''.join(page_str_list))
-
-    return render(request, 'pretty_list.html',
-                  {'queryset': queryset, 'search_data': search_data, 'page_string': page_string})
+    context = {'search_data': search_data,
+               'queryset': page_object.page_queryset,   # 分完页的数据
+               'page_string': page_object.html()        # 页码
+    }
+    return render(request, 'pretty_list.html', context)
 
 
     # # 搜索手机号方式一
