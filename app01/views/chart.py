83840b2dce352b06093fac5227c9c85e0228a924
Chen21yan <1170860535@qq.com>
2025-07-05 18:05:22 +0800

    echarts和highcharts

--- app01/views/chart.py
+++ app01/views/chart.py
@@ -33,3 +33,54 @@ def chart_bar(request):
         }
     }
     return JsonResponse(result)
+
+
+def chart_pie(request):
+    """ 构造饼图的数据 """
+
+    db_data_list = [
+        {"value": 2048, "name": '腾讯会议'},
+        {"value": 1735, "name": '腾讯元宝'},
+        {"value": 580, "name": '腾讯文档'},
+    ]
+
+    result = {
+        "status": True,
+        "data": db_data_list
+    }
+    return JsonResponse(result)
+
+
+def chart_line(request):
+    legend = ["上海", "深圳"]
+    series_list = [
+        {
+            "name": '上海',
+            "type": 'line',
+            "stack": 'Total',
+            "data": [15, 20, 36, 10, 10, 10]
+        },
+        {
+            "name": '深圳',
+            "type": 'line',
+            "stack": 'Total',
+            "data": [45, 10, 66, 40, 20, 50]
+        }
+    ]
+    x_axis = ['1月', '2月', '4月', '5月', '6月', '7月']
+
+    result = {
+        "status": True,
+        "data": {
+            'legend': legend,
+            'series_list': series_list,
+            'x_axis': x_axis,
+        }
+    }
+    return JsonResponse(result)
+
+
+def highcharts(request):
+    """ highcharts示例 """
+
+    return render(request, 'highcharts.html')
